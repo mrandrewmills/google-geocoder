b7130bc851e2c4625a17cc0d331ef91d39f958ee
Andrew Mills <AndrewMills@knowwwhow.com>
2015-01-17 11:36:40 -0500

    output format setter
    added error handling/exception to setOutputFormat()

--- googleGeocoder.class.php
+++ googleGeocoder.class.php
@@ -5,8 +5,8 @@
 	    private $geoCoordinates;
 	    private $limit = 5; // default value
 	    private $apiKey;
-	    private $outputFormat = "json"; // can be json or xml
-	    private $fullResponse;
+		private $outputFormat = "json"; // can be json or xml
+		private $fullResponse;
 	
 	    // Getters & Setters
 	    public function getStreetAddress() {
@@ -41,36 +41,47 @@
 	        $this->apiKey = $apiKey;
 	    }
 		
-	    public function getOutputFormat() {
-		return $this->outputFormat;
-	    }
+		public function getOutputFormat() {
+			return $this->outputFormat;
+		}
 		
-	    public function setOutputFormat($outputFormat) {
-		$this->outputFormat = $outputFormat;
-	    }
+		public function setOutputFormat($outputFormat) {
+				
+			$outputFormat = strtolower($outputFormat);
+			
+			if (($outputFormat != "json") or ($ouputFormat != "xml")) {
+				$errMsg = "Invalid format ($outputFormat) specified. Must be xml or json.";
+				throw new Exception($errMsg);
+			}
+			
+			// otherwise assign the value
+			$this->outputFormat = $outputFormat;
+		}
 	
 	    public function geocode() {
 	
 	        // Google geocoder has rate limit 
 	        usleep( 1000000 / $this->limit );
 
-		// let's build our URL
-		$baseURL = "https://maps.googleapis.com/maps/api/geocode/";
-		$output = $this->outputFormat;
-		$addressURL = "?address=" . rawurlencode($this->streetAddress);
-		$keyURL = "&key=" . $this->apiKey;
+			// let's build our URL
+			$baseURL = "https://maps.googleapis.com/maps/api/geocode/";
+			$output = $this->outputFormat;
+			$addressURL = "?address=" . rawurlencode($this->streetAddress);
+			$keyURL = "&key=" . $this->apiKey;
 
-		// now let's put them all together
-		$fullURL = $baseURL . $output . $addressURL . $keyURL;
+			// now let's put them all together
+			$fullURL = $baseURL . $output . $addressURL . $keyURL;
 	
-		// now let's ask Google for the geocode information	
-		$this->fullResponse = file_get_contents($fullURL);
-	
-		// ToDo: check status of the response, to make sure everything is good
-		// ToDo: convert response to array, parse for coordinates
+			// now let's ask Google for the geocode information	
+			$this->fullResponse = file_get_contents($fullURL);
 			
-		// return full response for right now.
-		return $this->fullResponse;
+			$geocoordinates = "";
+	
+			// ToDo: check status of the response, to make sure everything is good
+			// ToDo: convert response to array, parse for coordinates
+			
+			// return full response for right now.
+			return $this->fullResponse;
 	    }
 	}
 ?>
