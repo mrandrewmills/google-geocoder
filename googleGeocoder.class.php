190777b2eb52f62f73ec9b55d3e144dd44e889e0
mrandrewmills <andrewmills@knowwwhow.com>
2015-01-20 23:05:24 -0500

    geocode method tweak
    fix URL building process, fix issue in parsing

--- googleGeocoder.class.php
+++ googleGeocoder.class.php
@@ -63,27 +63,29 @@
 	        // Google geocoder has rate limit 
 	        usleep( 1000000 / $this->limit );
 
-			// let's build our URL
-			$baseURL = "https://maps.googleapis.com/maps/api/geocode/";
-			$output = $this->outputFormat;
-			$addressURL = "?address=" . rawurlencode($this->streetAddress);
-			$keyURL = "&key=" . $this->apiKey;
+		// let's build our URL
+		$baseURL = "https://maps.googleapis.com/maps/api/geocode/";
+		// $output = $this->outputFormat;
+		$baseURL .= $this->outputFormat;
+		$baseURL .= "?address=";
+		$baseURL .= rawurlencode($this->streetAddress);
+		$baseURL .= "&key=";
+		$baseURL .= $this->apiKey;
 
-			// now let's put them all together
-			$fullURL = $baseURL . $output . $addressURL . $keyURL;
+		// now let's put them all together
+		$fullURL = $baseURL . $output . $addressURL . $keyURL;
 	
-			// now let's ask Google for the geocode information	
-			$fullResponse = json_decode(file_get_contents($fullURL), true);
-			
-			$geocoordinates = "";
+		// now let's ask Google for the geocode information	
+		$fullResponse = json_decode(file_get_contents($baseURL), true);
+				
+		$geocoordinates = "";
 	
-			// ToDo: check status of the response, to make sure everything is good
+		// ToDo: check status of the response, to make sure everything is good
 			
-			$geocoordinates["latitude"] = $this->fullResponse["results"][0]["geometry"]["location"]["lat"];
-			$geocoordinates["longitude"] = $this->fullResponse["results"][0]["geometry"]["location"]["lng"];
-			
-			// return full response for right now.
-			return $geocoordinates;
+		$geocoordinates["latitude"] = $fullResponse["results"][0]["geometry"]["location"]["lat"];
+		$geocoordinates["longitude"] = $fullResponse["results"][0]["geometry"]["location"]["lng"];
+	
+		return $geocoordinates;
 	    }
 	}
 ?>
